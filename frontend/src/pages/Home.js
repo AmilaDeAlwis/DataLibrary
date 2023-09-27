2608f12d8435b15209d14f2f236dab5c6f6efe40
AmilaDeAlwis <amilaalwis4@gmail.com>
2023-09-27 14:21:04 +0530

    get data from db to client view

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,7 +1,27 @@
-const Home = () =>{
+import { useEffect, useState } from 'react'
+
+const Home = () => {
+    const [growthRate, setGrowthRate] = useState(null)
+
+    useEffect(() => {
+        const fetchGrowthRate = async () => {
+            const response = await fetch('/api/growthRate')
+            const json = await response.json()
+
+            if (response.ok){
+                setGrowthRate(json)
+            }
+        }
+        fetchGrowthRate()
+    }, [])
+
     return (
         <div className="home">
-            <h2>Home</h2>
+            <div className="growthRate">
+                {growthRate && growthRate.map((growthRate) => (
+                    <p key={growthRate._id}>{growthRate.title}</p>
+                ))}
+            </div>
         </div>
     )
 }
