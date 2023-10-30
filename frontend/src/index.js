8ab821d6bf4f8bbbb58161291d9d8f31e2d07e9b
AmilaDeAlwis <amilaalwis4@gmail.com>
2023-10-30 11:54:34 +0530

    react auth context added

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { GdpContextProvider } from './context/GdpContext';
+import { AuthContextProvider } from './context/AuthContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <GdpContextProvider>
-      <App />
-    </GdpContextProvider>
+    <AuthContextProvider>
+      <GdpContextProvider>
+        <App />
+      </GdpContextProvider>
+    </AuthContextProvider>
   </React.StrictMode>
 );
